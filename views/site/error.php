47e1a22e4f5a6a463da128ee461fae328a4d84bb
Dzenan Imamovic <imamovicdze@gmail.com>
2019-12-13 21:16:22 +0100

    added link to back on news from error page

--- views/site/error.php
+++ views/site/error.php
@@ -1,3 +1,8 @@
+<?php
+
+use yii\helpers\Url;
+
+?>
 <div class="st-content">
     <div class="container">
         <div class="row">
@@ -11,15 +16,8 @@
                             <p class="lead">Sorry, we could not found the page you are looking for!</p>
 
                             <div class="row">
-                                <div class="col-sm-4 col-sm-offset-4">
-                                    <form role="search" method="get" id="searchform1" action="#">
-                                        <div>
-                                            <input type="text" placeholder="Search and hit enter..." name="s" id="s"/>
-                                        </div>
-                                    </form>
-                                    <p class="go-back-home"><a href="index.html">
+                                    <p class="go-back-home"><a href="<?= Url::ToRoute(['/']); ?>">
                                             Back to Home Page</a></p>
-                                </div>
                             </div>
 
                         </section><!-- .error-404 -->
