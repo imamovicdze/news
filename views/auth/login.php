bb04022dc8c74d0e80edce5acc3fe0ca8eea1745
Dzenan Imamovic <imamovicdze@gmail.com>
2019-12-15 00:43:19 +0100

    make consistent sign-up & login page

--- views/auth/login.php
+++ views/auth/login.php
@@ -13,9 +13,7 @@ $this->params['breadcrumbs'][] = $this->title;
 <div class="container">
     <div class="site-login">
         <h1><?= Html::encode($this->title) ?></h1>
-
         <p>Please fill out the following fields to login:</p>
-
         <?php $form = ActiveForm::begin([
             'id' => 'login-form',
             'layout' => 'horizontal',
@@ -38,12 +36,6 @@ $this->params['breadcrumbs'][] = $this->title;
                     <?= Html::submitButton('Login', ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
                 </div>
             </div>
-
         <?php ActiveForm::end(); ?>
-
-<!--        <div class="col-lg-offset-1" style="color:#999;">-->
-<!--            You may login with <strong>admin/admin</strong> or <strong>demo/demo</strong>.<br>-->
-<!--            To modify the username/password, please check out the code <code>app\models\User::$users</code>.-->
-<!--        </div>-->
     </div>
 </div>
