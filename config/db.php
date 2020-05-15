82e19e75a8d61bf9a11e0cea0f655364be0cd8b2
Dzenan Imamovic <imamovicdze@gmail.com>
2020-05-15 23:53:50 +0200

    port fix

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=webapp',
+    'dsn' => 'mysql:host=localhost;port=3308;dbname=webapp',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
